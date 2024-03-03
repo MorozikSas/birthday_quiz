39d1e4210062f41ec0023f2775e4fe513e50615a
Синица Владислав Игоревич <Uladzislau.Sinitsa@alfa-bank.by>
2024-03-03 12:59:13 +0300

    gh-pages Add code validation and viev statuses

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const $inputs = document.querySelectorAll('input[type="text"]')
 const $button = document.querySelector('button')
+const $label = document.querySelector('body div > div label')
 
 $inputs.forEach(($input, key) => {
     $input.addEventListener('input', event => {
@@ -20,15 +21,19 @@ $inputs.forEach(($input, key) => {
     })
     $input.addEventListener('focus', () => {
         $input.selectionStart = 0
+        $input.classList = ''
+        $label.classList = ''
+        $label.innerText = ''
     })
 })
 
 $button.addEventListener('click', () => {
     const formData = new FormData()
-    const code = parseInt([...$inputs].map($input => $input.value).join(''))
-    const action = window.location.pathname.slice(0, -1).split('/')[3]
+    const code = parseInt([...$inputs].map($input => $input.value).join('')) || null
+    const action = window.location.pathname.replace('index.html', '').slice(0, -1).split('/')
+    console.log(action)
     formData.append('code', code)
-    formData.append('action', action)
+    formData.append('action', action[action.length - 1])
     fetch(
         'https://megaborodun4ik.monster/api/v1/birthday_quiz/',
         {
@@ -38,7 +43,12 @@ $button.addEventListener('click', () => {
     )
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            $inputs.forEach(($input, key) => {
+                data.status = JSON.parse(data.status)
+                $input.classList.add(`${data.status ? '' : 'in'}valid`)
+            })
+            $label.classList.add(`${data.status ? '' : 'in'}valid`)
+            $label.innerText = `${data.status ? '' : 'не '}верно`
         })
         .catch(error => {
             console.error('Error:', error)
